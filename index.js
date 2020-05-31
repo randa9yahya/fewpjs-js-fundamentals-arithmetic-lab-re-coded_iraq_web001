109c483d196e2afc010d9e384d690a64cd79aa20
randa yahya <randaalrayan96@gmial.com>
2020-05-31 17:22:58 +0000

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+
+var num1 =31 ;
+var num2 =2 ;
+var random 
+var mod 
+var multiply = num1*num2;
+let max =Math.max(1,2,3,20);
+random = Math.ceil(Math.random() * 10);
+mod = 16 % 6;
+
+
