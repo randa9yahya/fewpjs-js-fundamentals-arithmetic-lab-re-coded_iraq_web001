39dc1cf560335a817ce152e8a9c537e5c272d802
randa yahya <randaalrayan96@gmial.com>
2020-05-31 17:22:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ var num2 =2 ;
 var random 
 var mod 
 var multiply = num1*num2;
-var max = [1,2,3,20];
-random = Math.cilce(Math.random() * 10);
+ max =Math.max(1,2,3,20);
+random = Math.ceil(Math.random() * 10);
 mod = 16 % 6;
-Math.max(max);
-//Math.max([...]);
+
 
